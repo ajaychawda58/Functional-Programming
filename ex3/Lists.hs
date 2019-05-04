20f81cae6f49e5f7fdec8cfba2f0316ae88b0915
Nishan Tamang <nishantamang1337@gmail.com>
2019-05-04 21:00:14 +0200

    Update Lists.hs

--- ex3/Lists.hs
+++ ex3/Lists.hs
@@ -6,7 +6,13 @@ prod [] = 1
 prod (x:xs) = x * prod xs
 
 contains :: Integer -> [Integer] -> Bool
-contains = undefined
+contains y [] = False
+contains y (x:xs)
+    | y == x = True
+    | otherwise = contains y xs
+--contains x l
+--    | x `elem` l = True
+--    | otherwise = False
 
 nth :: Integer -> [a] -> Maybe a
 nth = undefined
