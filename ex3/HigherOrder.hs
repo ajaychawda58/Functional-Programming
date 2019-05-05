bfe283ca39ff01cfab86e277d36061414893d7df
Nishan Tamang <nishantamang1337@gmail.com>
2019-05-05 23:24:54 +0200

    Update HigherOrder.hs

--- ex3/HigherOrder.hs
+++ ex3/HigherOrder.hs
@@ -10,7 +10,7 @@ swap (a,b) = (b,a)
 g :: [(a, b)] -> [(b, a)]
 --g = undefinedg
 {--g [] = []
---g ((x,y): xs) = (y,x):g xs            --for some reason not passes testcases but works perfectly in mine
+--g ((x,y): xs) = (y,x):g xs            
 g [] = []
 g ((a,b):xs) = map swap [(a,b)]
 --}
