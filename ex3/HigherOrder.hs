ef47a862448fa194a26a883d78e2fc13f5288c31
Nishan Tamang <nishantamang1337@gmail.com>
2019-05-05 22:03:08 +0200

    Update HigherOrder.hs

--- ex3/HigherOrder.hs
+++ ex3/HigherOrder.hs
@@ -4,10 +4,15 @@ where
 f :: [(Bool, a)] -> [a]
 f = undefined
 
+swap :: (Integer,Integer) -> (Integer,Integer)
+swap (x,y) = (y,x)
+
 g :: [(a, b)] -> [(b, a)]
 --g = undefinedg
+--g [] = []
+--g ((x,y): xs) = (y,x):g xs
 g [] = []
-g ((x,y): xs) = (y,x):g xs
+g ((x,y):xs) = map swap [(x,y)]
 
 
 h :: [(Integer, a)] -> [a]
