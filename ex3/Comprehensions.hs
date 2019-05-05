5a89b3b7a2b4f8a9e6552aeabe6de38af714c782
Nishan Tamang <nishantamang1337@gmail.com>
2019-05-05 16:45:49 +0200

    Update Comprehensions.hs

--- ex3/Comprehensions.hs
+++ ex3/Comprehensions.hs
@@ -7,7 +7,9 @@ f [] = []
 f xs= [y | (x,y)<- xs, x == True]
 
 g :: [(a, b)] -> [(b, a)]
-g = undefined
+--g = undefined
+g [] = []
+g xs = [(x,y) | (x,y) <-xs, let p=x,let q=y,let x=q,let y=p]
 
 h :: [(Integer, a)] -> [a]
 h = undefined
