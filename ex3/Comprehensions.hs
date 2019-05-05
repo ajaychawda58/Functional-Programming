08bd15f562c51da2cc8a3d5b4f496f21863207eb
Nishan Tamang <nishantamang1337@gmail.com>
2019-05-05 17:10:10 +0200

    Update Comprehensions.hs

--- ex3/Comprehensions.hs
+++ ex3/Comprehensions.hs
@@ -14,4 +14,4 @@ g xs = [(x,y) | (x,y) <-xs, let p=x,let q=y,let x=q,let y=p]
 h :: [(Integer, a)] -> [a]
 --h = undefined
 --h xs = concat[y | (x,y) <- xs, x<-[1..x]]
-h xs = return.concat $ [y | (x,y) <- xs, x<-[1..x]]
+--h xs = return.concat $ [y | (x,y) <- xs, x<-[1..x]]
