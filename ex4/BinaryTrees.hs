db788a800ae790e22f5ed25aea6887cc657d64f5
Nishan Tamang <nishantamang1337@gmail.com>
2019-05-12 23:41:03 +0200

    Update BinaryTrees.hs

--- ex4/BinaryTrees.hs
+++ ex4/BinaryTrees.hs
@@ -14,10 +14,11 @@ reverseTree Empty = Empty
 reverseTree (Node x y z) = (Node (reverseTree z) y (reverseTree x))
 
 -- TODO Remove
-inorder :: Tree a -> [a]
+{--inorder :: Tree a -> [a]
 inorder Empty = []
 inorder (Node l x r) = inorder l ++ [x] ++ inorder r
 
 treeTest0 = Node Empty 4 Empty
 treeTest1 = Node Empty 4 (Node Empty 3 Empty)
 treeTest2 = Node (Node (Node Empty 1 Empty) 2 (Node Empty 3 Empty)) 4 (Node (Node Empty 7 Empty) 9 Empty)
+--}
