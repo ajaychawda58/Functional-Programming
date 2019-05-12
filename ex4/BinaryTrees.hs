b75b5e1348a51c5ce215e73ad42921966f222fc0
Nithin Vadukkumchery Rajendrakumar <n_vadukkum19@cs.uni-kl.de>
2019-05-12 21:42:49 +0200

    Fixing left corner case

--- ex4/BinaryTrees.hs
+++ ex4/BinaryTrees.hs
@@ -6,7 +6,7 @@ data Tree elem = Empty | Node (Tree elem) elem (Tree elem)
 
 left :: Tree a -> Maybe a
 left Empty = Nothing
-left (Node Empty a Empty) = Just a
+left (Node Empty a _) = Just a
 left (Node x _ _) = left x
 
 reverseTree :: Tree a -> Tree a
@@ -18,5 +18,6 @@ inorder :: Tree a -> [a]
 inorder Empty = []
 inorder (Node l x r) = inorder l ++ [x] ++ inorder r
 
-treeTest1 = Node Empty 4 Empty
+treeTest0 = Node Empty 4 Empty
+treeTest1 = Node Empty 4 (Node Empty 3 Empty)
 treeTest2 = Node (Node (Node Empty 1 Empty) 2 (Node Empty 3 Empty)) 4 (Node (Node Empty 7 Empty) 9 Empty)
