55d0cdbc5e4372e98ae134ae094a028c8342ef6b
Nithin Vadukkumchery Rajendrakumar <n_vadukkum19@cs.uni-kl.de>
2019-05-12 22:08:59 +0200

    Fold member corner case

--- ex4/Fold.hs
+++ ex4/Fold.hs
@@ -10,7 +10,7 @@ allFalse = foldl (\accumulative element -> accumulative && not element) True
 -- allFalse = foldr (\x y-> x && not y) True
 
 member :: (Eq a) => a -> [a] -> Bool
-member k = foldl (\ac el-> if ((not ac) && k==el) then True else False ) False 
+member k = foldl (\ac el-> if ((not ac) && k==el) then True else ac) False 
 
 smallest :: [Int] -> Int
 smallest = foldl (\ac el-> if ac > el then el else ac) (maxBound :: Int) 
