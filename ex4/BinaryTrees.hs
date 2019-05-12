68c095b30800e4e096a538bda29e897780a17ee1
Nishan Tamang <nishantamang1337@gmail.com>
2019-05-12 17:04:55 +0200

    Update BinaryTrees.hs

--- ex4/BinaryTrees.hs
+++ ex4/BinaryTrees.hs
@@ -5,7 +5,11 @@ data Tree elem = Empty | Node (Tree elem) elem (Tree elem)
     deriving (Eq,Show)
 
 left :: Tree a -> Maybe a
-left = undefined
+--left = undefined
+left (Node EmptyTree EmptyTree EmptyTree) = Nothing
+left (Node EmptyTree a _) = Just a
+left (Node x _ _) = left x
+
 
 reverseTree :: Tree a -> Tree a
 reverseTree = undefined
