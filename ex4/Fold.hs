5cc12ee89b7d706b5fb06011f59b87008cbfe3ad
Nithin Vadukkumchery Rajendrakumar <n_vadukkum19@cs.uni-kl.de>
2019-05-12 22:04:40 +0200

    Fold first draft

--- ex4/Fold.hs
+++ ex4/Fold.hs
@@ -2,16 +2,21 @@ module Fold
 where
 
 allTrue :: [Bool] -> Bool
-allTrue = undefined
+allTrue = foldl (&&) True
+-- allTrue = foldr (&&) True
 
 allFalse :: [Bool] -> Bool
-allFalse = undefined
+allFalse = foldl (\accumulative element -> accumulative && not element) True
+-- allFalse = foldr (\x y-> x && not y) True
 
 member :: (Eq a) => a -> [a] -> Bool
-member = undefined
+member k = foldl (\ac el-> if ((not ac) && k==el) then True else False ) False 
 
 smallest :: [Int] -> Int
-smallest = undefined
+smallest = foldl (\ac el-> if ac > el then el else ac) (maxBound :: Int) 
 
 largest :: [Int] -> Int
-largest = undefined
+largest = foldl (\ac el-> if ac < el then el else ac) (minBound :: Int) 
+
+-- If both recursion schemes are applicable, which one is preferable in terms of running time?
+-- foldl' is tail recursive so foldl' is more space and time efficient than foldr.
