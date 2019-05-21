4b2ed8e88c1f3d0d4a49137e00dc90e832809df0
Nishan Tamang <nishantamang1337@gmail.com>
2019-05-21 08:39:28 +0200

    Update MapReduce.hs

--- ex5/MapReduce.hs
+++ ex5/MapReduce.hs
@@ -11,7 +11,9 @@ Please write your answer in this multiline comment.
 
 -- b) top-down
 foldmTD :: (a -> a -> a) -> a -> [a] -> a
-foldmTD = undefined
+--foldmTD = undefined
+foldmTD f e [] = return e
+foldmTD f e (x: xs) = do {y <- f e x; foldm f y xs}
 
 
 -- c) bottom-up
