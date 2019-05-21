64082ac44b5985f4d15f376f9ce11f6b70af3318
Nishan Tamang <nishantamang1337@gmail.com>
2019-05-21 08:46:54 +0200

    Update MapReduce.hs

--- ex5/MapReduce.hs
+++ ex5/MapReduce.hs
@@ -13,7 +13,7 @@ Please write your answer in this multiline comment.
 foldmTD :: (a -> a -> a) -> a -> [a] -> a
 --foldmTD = undefined
 --foldmTD f e Empty =  e
-foldmTD f e x = do {y <- f e x; foldmTD f y x}
+--foldmTD f e x = do {y <- f e x; foldmTD f y x}
 
 
 -- c) bottom-up
