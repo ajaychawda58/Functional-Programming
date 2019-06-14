3d12b1a99d07d5c6edf03667d642575bd41a12b6
Sebastian Schweizer <schweizer@cs.uni-kl.de>
2019-06-14 15:59:17 +0200

    ex8 [ci skip]

---
+++ ex8/Evaluator.hs
@@ -0,0 +1,29 @@
+module Evaluator
+where
+
+infixl 6 :+:
+infixl 7 :*:
+infixr 1 :?:
+
+data Expr
+  = Lit Integer    -- a literal
+  | Expr :+: Expr  -- addition
+  | Expr :*: Expr  -- multiplication
+  | Div Expr Expr  -- integer division
+  | Expr :?: Expr  -- non-deterministic choice
+
+-- Evaluator from the lecture (slide 421)
+evalA :: (Applicative f) => Expr -> f Integer
+evalA (Lit i)     = pure i
+evalA (e1 :+: e2) = pure (+)  <*> evalA e1 <*> evalA e2
+evalA (e1 :*: e2) = pure (*)  <*> evalA e1 <*> evalA e2
+evalA (Div e1 e2) = pure div  <*> evalA e1 <*> evalA e2
+evalA (_ :?: _)   = error "not implemented"
+
+-- Example for non-deterministic expression
+toss :: Expr
+toss = Lit 0 :?: Lit 1
+
+-- Your task: Implement this function
+evalN :: Expr -> [Integer]
+evalN = undefined
