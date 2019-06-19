74d526e654aa6908217a72a1a027630b67d9c807
Nishan Tamang <nishantamang1337@gmail.com>
2019-06-19 23:46:44 +0200

    Update WordCount.hs

--- ex8/WordCount.hs
+++ ex8/WordCount.hs
@@ -5,6 +5,27 @@ where
 import System.Environment
 
 main :: IO ()
+--main = do
+--    args <- getArgs
+--    putStrLn "To be done..."
+countLines :: String -> Int
+countLines str = length.lines $ str
+
+countWords :: String -> Int
+countWords str=length.words $ str
+
+countBytes :: String -> Int
+countBytes str=length $ str
+
+conv :: [Char] -> String
+conv s = s
+
 main = do
-    args <- getArgs
-    putStrLn "To be done..."
+    [args] <- getArgs
+    contents <- readFile args
+    let x=conv contents
+    let fileName=conv args
+    let a=countLines x
+    let b=countWords x
+    let c=countBytes x
+    putStrLn ((show (a)) ++ " " ++ (show (b)) ++ " " ++ (show (c))++ " " ++ fileName ++ "\n")
