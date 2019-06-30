1d3f1086a7496a518cc963e10b8aa9a9857e3484
Nishan Tamang <nishantamang1337@gmail.com>
2019-06-30 23:15:20 +0200

    Update Zeroless.hs

--- ex9/Zeroless.hs
+++ ex9/Zeroless.hs
@@ -18,8 +18,25 @@ size (Two a n) = 2 + 2*size n
 
 -- b)
 cons :: elem -> Sequ elem -> Sequ elem
-cons = undefined
-
+--cons = undefined
+cons a b = case b of
+  Nil    -> One a Nil
+  One y ys -> Two $ cons (Pair a y) ys
+  Two (One y ys) -> One $ (Two $ cons (Pair a y ) ys)
+{-
+0
+1
+2
+11
+12
+21
+22
+111
+112
+121
+.
+.
+-}
 -- c)
 head :: Sequ elem -> elem
 head = undefined
